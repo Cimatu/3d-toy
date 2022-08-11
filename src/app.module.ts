50e1ee1251fb40fd04280f23502857d96698e8a1
pavelgurinovich <pavel.gurinovich@yoffix.com>
2022-08-11 03:00:37 +0300

    App module changed

--- src/app.module.ts
+++ src/app.module.ts
@@ -35,7 +35,7 @@ import UserDataModule from './events/usersData/usersData.module';
       type: 'postgres',
       host: process.env.POSTGRES_HOST,
       port: Number(process.env.POSTGRES_PORT),
-      username: process.env.POSTGRES_USER,
+      username: process.env.POSTGRES_USERNAME,
       password: process.env.POSTGRES_PASSWORD,
       database: process.env.POSTGRES_DATABASE,
       entities: [User, Car, Cart, CartItem, Order, Wheel, Spoiler, Toning, Detail, BallBearingSet, Gear, BodyCar, Event, UserData],
