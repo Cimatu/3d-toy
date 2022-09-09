1b78b5db199a685871f1d021a66051170c68794e
pavelgurinovich <pavel.gurinovich@yoffix.com>
2022-09-09 04:22:52 +0300

    Cors fix

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,15 @@ const start = async () => {
   const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
 
+  // const options = {
+  //   "origin": "*",
+  //   "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+  //   "preflightContinue": false,
+  //   "optionsSuccessStatus": 204,
+  //   "credentials": true
+  // }
+  app.enableCors();
+
   const config = new DocumentBuilder()
     .setTitle(`3D Toy Cars`)
     .setDescription(`REST API Docs`)
@@ -16,4 +25,5 @@ const start = async () => {
   SwaggerModule.setup('/api/docs', app, document)
   await app.listen(PORT, () => console.log(`Server started on PORT = ${PORT}`));
 }
+
 start();
